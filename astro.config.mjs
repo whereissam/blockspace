4addcd941b4d5065b183d892317ac483b520e4d3
whereissam <sam135642@gmail.com>
2024-02-10 19:56:48 +0800

    feat: add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,23 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from "astro-icon"
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://block-spaces.netlify.app/',
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
+  integrations: [
+    mdx(), 
+    icon(), 
+    tailwind({
     applyBaseStyles: false,
-  }), compress()],
+  }), 
+  compress(),
+  partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),
+],
 })
