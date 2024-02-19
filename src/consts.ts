ebfee9163242739b75f79dda291b7062887fb1f6
whereissam <sam135642@gmail.com>
2024-02-20 00:43:44 +0800

    feat: change site metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '區塊空間部落格';
+export const SITE_DESCRIPTION = '歡迎來到我的部落格！';
