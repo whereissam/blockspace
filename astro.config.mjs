c69b32f3428b4ebcb9342f620865bcdd5be46154
whereissam <sam135642@gmail.com>
2024-03-03 13:58:33 +0800

    feat: add nwe css library & fix btn color

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,21 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from "astro-icon"
-import partytown from '@astrojs/partytown'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from "astro-icon";
+import partytown from '@astrojs/partytown';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://block-spaces.netlify.app/',
   compressHTML: true,
-  integrations: [
-    mdx(), 
-    icon(), 
-    tailwind({
-    applyBaseStyles: false,
-  }), 
-  compress(),
-  partytown({
+  integrations: [mdx(), icon(), tailwind({
+    applyBaseStyles: false
+  }), compress(), partytown({
     config: {
-      forward: ["dataLayer.push"],
-    },
-  }),
-],
-})
+      forward: ["dataLayer.push"]
+    }
+  }), react()]
+});
