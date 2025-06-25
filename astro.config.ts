d97ca474685b9eda3b6b0621def00d30617ecc14
whereissam <sam135642@gmail.com>
2025-06-25 19:18:05 +0200

    chore: update site URL in Astro config, modify local settings for additional Bash commands, and change footer sitemap link

--- astro.config.ts
+++ astro.config.ts
@@ -120,7 +120,7 @@ export default defineConfig({
   // https://docs.astro.build/en/guides/prefetch/
   prefetch: true,
   // ! Please remember to replace the following site property with your own domain
-  site: "http://astrocitrus.artemkutsan.pp.ua/",
+  site: "https://blockspaces.xyz/",
   vite: {
     build: {
       sourcemap: true, // Source maps generation
